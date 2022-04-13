5c76ca33b1a5ffbbfa326160a52c286318598662
Jeff Cadieux <jeffrey@r.recruit.co.jp>
2022-04-13 12:10:28 +0900

    Create branch for new references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,32 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { Todo } from './models/types';
 import { initialData } from './data/initial-data';
-import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // public todos: Todo[] = initialData;
-  public todosLocal: Todo[] = initialData;
-  public todos = new BehaviorSubject(this.todosLocal);
-  public title = 'change-detection-demo';
+  public todos: Todo[] = initialData;
 
   public toggleFirst() {
-    // this.todos[0].isCompleted = !this.todos[0].isCompleted;
-    // const newTodos = [...this.todos];
-    // newTodos[0].isCompleted = !newTodos[0].isCompleted;
-    // this.todos = newTodos;
+    const newTodos = [...this.todos];
+    newTodos[0].isCompleted = !newTodos[0].isCompleted;
+    this.todos = newTodos;
   }
 
   public addTodo() {
-    // this.todos.push({
-    //   id: 3,
-    //   description: 'The third to-do item',
-    //   isCompleted: false,
-    // });
-    // const newTodos = [...this.todos];
-    // newTodos.push({
-    //   id: 3,
-    //   description: 'The third to-do item',
-    //   isCompleted: false,
-    // });
-    // this.todos = newTodos;
-    this.todosLocal.push({
+    const newTodos = [...this.todos];
+    newTodos.push({
       id: 3,
       description: 'The third to-do item',
       isCompleted: false,
     });
-    this.todos.next(this.todosLocal);
+    this.todos = newTodos;
   }
 
   public onToggle(id: number) {
-    // const todoToToggle = this.todos.find((todo) => todo.id === id)!;
-    // todoToToggle.isCompleted = !todoToToggle.isCompleted;
+    const todoToToggle = this.todos.find((todo) => todo.id === id)!;
+    todoToToggle.isCompleted = !todoToToggle.isCompleted;
   }
 }
--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,13 +1,10 @@
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   EventEmitter,
   Input,
-  OnInit,
   Output,
 } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Todo } from '../models/types';
 
 @Component({
@@ -16,23 +13,9 @@ import { Todo } from '../models/types';
   styleUrls: ['./to-do-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ToDoListComponent implements OnInit {
-  // @Input() todos: Todo[] = [];
-  @Input('todos') newData!: Observable<Todo[]>;
-  public todos: Todo[] = [];
+export class ToDoListComponent {
+  @Input() todos: Todo[] = [];
   @Output() toggle = new EventEmitter<number>();
-
-  constructor(private changeDetectorRef: ChangeDetectorRef) {}
-
-  ngOnInit(): void {
-    // setInterval(() => {
-    //   this.changeDetectorRef.markForCheck();
-    // }, 5000);
-    this.newData.subscribe((data) => {
-      this.todos = data;
-      this.changeDetectorRef.markForCheck();
-    });
-  }
 
   public onToggle(id: number) {
     this.toggle.emit(id);
