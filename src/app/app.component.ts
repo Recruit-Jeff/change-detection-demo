87da58ba48bf8b606bbd4ff53cd86ad61f63db11
Jeff Cadieux <jeffrey@r.recruit.co.jp>
2022-04-13 12:05:31 +0900

    Create branch for onPush change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,28 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { Todo } from './models/types';
 import { initialData } from './data/initial-data';
-import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // public todos: Todo[] = initialData;
-  public todosLocal: Todo[] = initialData;
-  public todos = new BehaviorSubject(this.todosLocal);
-  public title = 'change-detection-demo';
+  public todos: Todo[] = initialData;
 
   public toggleFirst() {
-    // this.todos[0].isCompleted = !this.todos[0].isCompleted;
-    // const newTodos = [...this.todos];
-    // newTodos[0].isCompleted = !newTodos[0].isCompleted;
-    // this.todos = newTodos;
+    this.todos[0].isCompleted = !this.todos[0].isCompleted;
   }
 
   public addTodo() {
-    // this.todos.push({
-    //   id: 3,
-    //   description: 'The third to-do item',
-    //   isCompleted: false,
-    // });
-    // const newTodos = [...this.todos];
-    // newTodos.push({
-    //   id: 3,
-    //   description: 'The third to-do item',
-    //   isCompleted: false,
-    // });
-    // this.todos = newTodos;
-    this.todosLocal.push({
+    this.todos.push({
       id: 3,
       description: 'The third to-do item',
       isCompleted: false,
     });
-    this.todos.next(this.todosLocal);
   }
 
   public onToggle(id: number) {
-    // const todoToToggle = this.todos.find((todo) => todo.id === id)!;
-    // todoToToggle.isCompleted = !todoToToggle.isCompleted;
+    const todoToToggle = this.todos.find((todo) => todo.id === id)!;
+    todoToToggle.isCompleted = !todoToToggle.isCompleted;
   }
 }
